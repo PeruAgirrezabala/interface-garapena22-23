9e3f3c9982959329c56afc9ffc600eab3db3b4a7
Agirrezabala Nieto <agirrezabala.peru@uni.lan>
2022-11-22 12:08:52 +0100

    eguneratzea

---
+++ C#/Adibidea/Adibidea/Controllers/KaixoMunduaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace Adibidea.Controllers
+{
+    public class KaixoMunduaController : Controller
+    {
+
+        public ActionResult OngiEtorri(string izena, int
+        zenbat = 1)
+        {
+            ViewBag.Mezua = "Hello " + izena;
+            ViewBag.Zenbat = zenbat;
+            return View();
+        }
+
+
+    }
+}
